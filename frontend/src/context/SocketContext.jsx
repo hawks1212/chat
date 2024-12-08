1b98cbae48f27091a1d0809b7f55016bdc26cbb1
simon <simonkatich0037@gmail.com>
2024-12-08 12:00:22 +0530

    socket update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatss-y6pz.onrender.com", {
+			const socket = io("https://chatss-y6pz.onrender.com/login", {
 				query: {
 					userId: authUser._id,
 				},
